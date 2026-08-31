faea286d7307656b502413a1aa0ddef1c284a51c
Francois <francois.hoyez@gmail.com>
2026-08-31 14:08:27 +0200

    Ajouter l’icône du plugin Unraid

--- unraid-plugin/package.sh
+++ unraid-plugin/package.sh
@@ -24,7 +24,7 @@ esac
 mkdir -p \
     "$stage_dir/etc/rc.d" \
     "$stage_dir/install" \
-    "$stage_dir/usr/local/emhttp/plugins/unraid-vsock-sensors" \
+    "$stage_dir/usr/local/emhttp/plugins/unraid-vsock-sensors/images" \
     "$stage_dir/usr/local/sbin" \
     "$dist_dir"
 
@@ -37,6 +37,8 @@ mkdir -p \
 install -m 0755 "$plugin_dir/rc.unraid-vsock-sensors" "$stage_dir/etc/rc.d/rc.unraid-vsock-sensors"
 install -m 0644 "$plugin_dir/UnraidVsockSensors.page" \
     "$stage_dir/usr/local/emhttp/plugins/unraid-vsock-sensors/UnraidVsockSensors.page"
+install -m 0644 "$plugin_dir/images/icon.png" \
+    "$stage_dir/usr/local/emhttp/plugins/unraid-vsock-sensors/images/icon.png"
 install -m 0644 "$plugin_dir/default.cfg" \
     "$stage_dir/usr/local/emhttp/plugins/unraid-vsock-sensors/default.cfg"
 install -m 0644 "$repo_dir/LICENSE" \
